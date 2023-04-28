12802cb342d3fceb18010eb6bcec8ad94a55c095
ghousek1 <ghousek1@outlook.com>
2023-04-28 20:57:21 +0530

    font size changed

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -5,7 +5,7 @@ function ComingSoon() {
     <>
     <div
     className="w-full h-screen bg-gradient-to-bl from-orange-500 to-yellow-300 flex flex-col justify-center items-center text-white">
-    <h1 className="text-5xl"> <b>Viscody</b> </h1>
+    <h1 className="text-[3rem]"> <b>Viscody</b> </h1>
     <p>Stay tuned for something amazing!!!</p>
 
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 mt-10 lg:mt-20">
